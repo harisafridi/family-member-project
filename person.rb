6dd0d3f100eb159af4f38540c599c3a47f65c5d3
Haris Mohammad <hmohammad@spartaglobal.com>
2015-11-18 17:04:18 +0000

    added .to_s checker

--- person.rb
+++ person.rb
@@ -1,19 +1,30 @@
 require 'date'
 class Person
- attr_accessor   :first_name, :last_name,:dob, :emailarray
+ attr_accessor   :first_name, :last_name,:dob, :emailarray, :phonearray
 	def initialize(fname, sname, dob)
 		@dob = Date.parse(dob)
 		@first_name = fname.capitalize
 		@last_name = sname.capitalize
 		@emailarray = []
+		@phonearray = []
 	end
 	def full_name	
-		"#{first_name} #{last_name}"
+	 "#{first_name} #{last_name}"
 	end
 	def add_email(emailarray)
     @emailarray << emailarray
-   end
-   def show_email
-    @emailarray.inspect
-  end
+   	end
+   	def remove_email(index_position)
+    emailarray.delete_at(index_position)
+    end
+    def remove_phone(index_position)
+    phonearray.delete_at(index_position)
+    end
+  	def add_phone(phonearray)
+    @phonearray << phonearray
+    end
+    def to_s
+    	"Joe Bloggs was born on #{dob}.\n\ Their email addresses are: #{emailarray}.\n\ 
+    	Their phone numbers are #{phonearray}"
+    end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -10,11 +10,19 @@ describe "Person Class - enteries in phonebook" do
   expect(person.full_name).to eq "Joe Bloggs"
   end
 #overview of tests....
-  it "should add or delete inforamation" do
+  it "should add or delete email and phone details" do
 	person = Person.new("joe", "bloggs", "1 Jan 1990")
 	expect(person.emailarray).to eq []
-	person.add_email "joe@hotmail.com"
 	person.add_email "haris@hotmail.com"
-	expect(person.emailarray).to eq (["joe@hotmail.com","haris@hotmail.com"])
+  person.add_email "joe@foo.com"
+	expect(person.emailarray).to eq (["haris@hotmail.com","joe@foo.com"])
+  person.remove_email(0)
+  person.add_phone "0757577575"
+  person.add_phone "239423480424"
+  person.remove_phone(0)
+  expect(person.phonearray).to eq (["239423480424"])
+  #expect(person.to_s).to eq (1990-01-01)
+  it "should check whether .to_s contains right information" do
+  expect(person.to_s).to include("239423480424", "1990-01-01")
   end
 end

a653bb98464f09271140ee9489f614546b47f53b
Haris Mohammad <hmohammad@spartaglobal.com>
2015-11-18 16:01:20 +0000

    added tests for adding email

--- person.rb
+++ person.rb
@@ -1,13 +1,19 @@
 require 'date'
 class Person
- attr_accessor   :first_name, :last_name,:dob
+ attr_accessor   :first_name, :last_name,:dob, :emailarray
 	def initialize(fname, sname, dob)
 		@dob = Date.parse(dob)
 		@first_name = fname.capitalize
 		@last_name = sname.capitalize
-
+		@emailarray = []
 	end
 	def full_name	
 		"#{first_name} #{last_name}"
 	end
+	def add_email(emailarray)
+    @emailarray << emailarray
+   end
+   def show_email
+    @emailarray.inspect
+  end
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,12 +2,19 @@ require 'spec_helper'
 
 describe "Person Class - enteries in phonebook" do 
 #overview of tests....
-  it "should srtore and return personal information" do
+  it "should store and return personal information" do
 	person = Person.new("joe", "bloggs", "1 Jan 1990")
-  
   expect(person.dob.to_s).to eq "1990-01-01"
   expect(person.first_name).to eq "Joe"
   expect(person.last_name).to eq "Bloggs"
   expect(person.full_name).to eq "Joe Bloggs"
+  end
+#overview of tests....
+  it "should add or delete inforamation" do
+	person = Person.new("joe", "bloggs", "1 Jan 1990")
+	expect(person.emailarray).to eq []
+	person.add_email "joe@hotmail.com"
+	person.add_email "haris@hotmail.com"
+	expect(person.emailarray).to eq (["joe@hotmail.com","haris@hotmail.com"])
   end
 end
